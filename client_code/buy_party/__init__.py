940d18c7ad9724042d8d2ef445a9fec779b10043
oaadmin@protonmail.com <oaadmin@protonmail.com>
2024-02-19 03:35:57 +0000

    Edited buy_party

--- client_code/buy_party/__init__.py
+++ client_code/buy_party/__init__.py
@@ -9,5 +9,7 @@ class buy_party(buy_partyTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.link_1.url = "https://widget.piteas.io/#/swap?inputCurrency=PLS&outputCurrency=0x4581AF35199BBde87a89941220e04E27ce4b0099&theme=dark&exactField=input&exactAmount=10000000"
+    self.link_1_copy.url = "https://swap.defillama.com/?chain=ethereum&from=0x0000000000000000000000000000000000000000&to=0x4581AF35199BBde87a89941220e04E27ce4b0099"
 
     # Any code you write here will run before the form opens.
